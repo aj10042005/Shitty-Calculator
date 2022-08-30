2f469c56e7576f397f5819ee7003c4020418c93d
jerkyBeef <65261620+aj10042005@users.noreply.github.com>
2022-08-30 18:17:13 +1000

    Update Main.java

--- rpnCalc/src/Main.java
+++ rpnCalc/src/Main.java
@@ -7,8 +7,6 @@ import java.io.InputStreamReader;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        String exp = new BufferedReader(new InputStreamReader(System.in)).readLine();
-        System.out.println(Calculator.toReversePolishNotation(exp.toCharArray()));
-        System.out.println(Calculator.calc(exp));
+        System.out.println(Calculator.calc(new BufferedReader(new InputStreamReader(System.in)).readLine()));
     }
 }
